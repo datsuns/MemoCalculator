56b6f58196edb1b8648ce5b1c5b806d66dee4474
datsuns <the.skeleton7@gmail.com>
2013-03-04 00:33:34 +0900

    GUIの大きさを調整

--- Resources/ui/common/FirstView.js
+++ Resources/ui/common/FirstView.js
@@ -29,8 +29,8 @@ function CreateButton( text, top, left ){
 	var button = Ti.UI.createButton({
 		title: text,
 		color: '#000',
-		height: '50',
-		width: '50'
+		height: '15%',
+		width: '15%'
 	});
 	button.setTop( top )
 	button.setLeft( left )
@@ -110,28 +110,28 @@ function CreateFunctionButton( text, top, left, output ){
 
 function AddKeyButtons( parent, output ){
 	var buttons = [
-		CreateNumberButton('7', 50,  0, output),
-		CreateNumberButton('8', 50,  50, output),
-		CreateNumberButton('9', 50,  100, output),
-		CreateFunctionButton('/', 50,  150, output),
+		CreateNumberButton('7', '10%',  '0%', output),
+		CreateNumberButton('8', '10%',  '15%', output),
+		CreateNumberButton('9', '10%',  '30%', output),
+		CreateFunctionButton('/', '10%',  '45%', output),
 
-		CreateNumberButton('4', 100, 0, output),
-		CreateNumberButton('5', 100, 50, output),
-		CreateNumberButton('6', 100, 100, output),
-		CreateFunctionButton('*', 100, 150, output),
+		CreateNumberButton('4', '25%', '0%', output),
+		CreateNumberButton('5', '25%', '15%', output),
+		CreateNumberButton('6', '25%', '30%', output),
+		CreateFunctionButton('*', '25%', '45%', output),
 
-		CreateNumberButton('1', 150, 0, output),
-		CreateNumberButton('2', 150, 50, output),
-		CreateNumberButton('3', 150, 100, output),
-		CreateFunctionButton('-', 150, 150, output),
+		CreateNumberButton('1', '40%', '0%', output),
+		CreateNumberButton('2', '40%', '15%', output),
+		CreateNumberButton('3', '40%', '30%', output),
+		CreateFunctionButton('-', '40%', '45%', output),
 
-		CreateNumberButton('0', 200, 0, output),
-		CreateFunctionButton('.', 200, 50, output),
-		CreateFunctionButton('=', 200, 100, output),
-		CreateFunctionButton('+', 200, 150, output),
+		CreateNumberButton('0', '55%', '0%', output),
+		CreateFunctionButton('.', '55%', '15%', output),
+		CreateFunctionButton('=', '55%', '30%', output),
+		CreateFunctionButton('+', '55%', '45%', output),
 
-		CreateFunctionButton('c', 250, 0, output),
-		CreateFunctionButton('debug', 250, 150, output),
+		CreateFunctionButton('c', '70%', '0%', output),
+		CreateFunctionButton('debug', '70%', '15%', output),
 	]
 
 	for( var i = 0; i < buttons.length; i++){
@@ -152,8 +152,8 @@ function FirstView() {
 	var label = Ti.UI.createLabel({
 		color:'#000000',
 		text:'0',
-		height:50,
-		width:200,
+		height:'10%',
+		width:'50%',
 		top:0,
 		left:0,
 		textAlign: Ti.UI.TEXT_ALIGNMENT_RIGHT,
@@ -163,10 +163,10 @@ function FirstView() {
 	AddKeyButtons( self, label )
 
 	var text_input = Ti.UI.createTextArea({
-		top:300,
+		top:'85%',
 		left:0,
-		height:200,
-		width:200,
+		height:'15%',
+		width:'50%',
 	})
 	self.add(text_input)
 	
